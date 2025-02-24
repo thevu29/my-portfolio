d5db83423cc0733dad69d9f0b27a17e2d9204b50
thevu29 <vnguyen132az@gmail.com>
2025-02-24 08:02:34 +0700

    init

---
+++ app/page.js
@@ -0,0 +1,83 @@
+import { userData } from "@/data/user-data";
+import Contributions from "./components/contributions";
+import HeroSection from "./components/hero-section";
+import GitLanguage from "./components/language";
+import Projects from "./components/projects";
+import Rank from "./components/rank";
+import GitStats from "./components/stats";
+
+async function getGitProfile() {
+  const res = await fetch(
+    `https://api.github.com/users/${userData.githubUser}`
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return await res.json();
+}
+
+async function getGitProjects() {
+  const res = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        {
+          user(login: "thevu29") {
+            pinnedItems(first: 6, types: REPOSITORY) {
+              nodes {
+                ... on Repository {
+                  id
+                  name
+                  description
+                  url
+                  stargazerCount
+                  primaryLanguage {
+                    name
+                    color
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+    }),
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+  return data.data.user.pinnedItems.nodes;
+}
+
+export default async function Home() {
+  const profile = await getGitProfile();
+  const projects = await getGitProjects();
+
+  return (
+    <>
+      <HeroSection profile={profile} />
+      <GitStats />
+      <Projects projects={projects} profile={profile} />
+      <GitLanguage />
+      <Rank />
+      <Contributions />
+    </>
+  );
+}
+
+export async function generateMetadata({ params, searchParams }, parent) {
+  const profile = await getGitProfile();
+
+  return {
+    title: `GitHub Profile of ${profile.name}`,
+    description: profile.description,
+  };
+}
---
+++ data/user-data.js
@@ -0,0 +1,25 @@
+export const userData = {
+  githubUser: "thevu29",
+  devUsername: "thevu29",
+  github: "https://github.com/thevu29",
+  facebook: "https://web.facebook.com/thevu29",
+  linkedIn: "https://www.linkedin.com/in/thevu29/",
+  // twitter: 'https://twitter.com/said7388',
+  // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  // leetcode: "https://leetcode.com/said3812/",
+  resume:
+    "https://drive.google.com/file/d/1-BxrUHet2SRT49Iburr_FVm6g17jcGRV/view?usp=sharing",
+  skills: [
+    "React",
+    "NextJS",
+    "Redux",
+    "Express",
+    "Java",
+    "Spring Boot",
+    "MySql",
+    "MongoDB",
+    "Postgres",
+    "Docker",
+  ],
+  timezone: "+7",
+};
